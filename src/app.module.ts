dc50678631d670bb7968c54574bf3af5c04069ea
goldbearman <sabirovfarit@gmail.com>
2023-03-10 17:34:03 +0300

    dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Controller, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MOMGO_CONNECTION),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/books/books.module.ts
+++ src/books/books.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BooksService } from './books.service';
 import { BooksController } from './books.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+import {Book} from "./interface/book.interface";
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:Book, schema: Book}])]
   controllers: [BooksController],
   providers: [BooksService],
   exports: [BooksService],
