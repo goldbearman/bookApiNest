d68a7fa4b227aa130b865da75d4620dde22b3fab
goldbearman <sabirovfarit@gmail.com>
2023-03-19 21:24:46 +0300

    Interceptor

---
+++ src/app.logging.interceptor.ts
@@ -0,0 +1,35 @@
+import {
+    CallHandler,
+    Injectable,
+    NestInterceptor,
+    ExecutionContext,
+    InternalServerErrorException
+} from '@nestjs/common';
+import {Reflector} from '@nestjs/core';
+import {Observable, throwError} from 'rxjs';
+import {tap, catchError} from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+    constructor() {}
+
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+        console.log('New request!');
+        const now = Date.now();
+        return next
+            .handle()
+            .pipe(
+                tap(() => {
+                    console.log(`\nExecution time: ${Date.now() - now}ms`);
+                    console.log('\nRequest was successful!');
+                }),
+                catchError(err => {
+                    console.log(`\nExecution time: ${Date.now() - now}ms`);
+                    console.log('\nRequest was failed!');
+                    console.log('\nError message: ', err);
+                    return throwError(new InternalServerErrorException());
+                })
+            );
+    }
+}
+
